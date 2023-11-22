530f4a15261fc802e5e37c14ebbc072a71a2566d
Anik Barua <anikbb05@gmail.com>
2023-11-22 22:33:03 +0600

    connect server with database

--- src/server.ts
+++ src/server.ts
@@ -1,26 +1,15 @@
 import mongoose from "mongoose";
 import app from "./app";
-
-const port = 5000;
-
-// main().catch((err) => console.log(err));
+import config from "./app/config";
 
 async function main() {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/test");
-    app.listen(port, () => {
-      console.log(`Server is running successfully on ${port}`);
+    await mongoose.connect(config.database_url as string);
+    app.listen(config.port, () => {
+      console.log(`Server is running successfully on ${config.port}`);
     });
   } catch (error) {
     console.log(error);
   }
 }
 main();
-
-// echo "# next-assignment-2" >> README.md
-//   git init
-//   git add README.md
-//   git commit -m "first commit"
-//   git branch -M main
-//   git remote add origin https://github.com/1104Anikbarua/next-assignment-2.git
-//   git push -u origin main
